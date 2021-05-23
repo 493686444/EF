24d1c7bf3ad1e7ea2eea4a0a956b34efc4d3c178
Gabrial <493686444@qq.com>
2021-05-23 21:20:26 +0800

    email and user

--- EF/Email.cs
+++ EF/Email.cs
@@ -6,7 +6,7 @@ namespace EF
 {
    public class Email
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Body { get; set; }
         public User User { get; set; }
     }
--- EF/User.cs
+++ EF/User.cs
@@ -6,7 +6,7 @@ namespace EF
 {
     public class User
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Name { get; set; }
         public string Password { get; set; }
     }
