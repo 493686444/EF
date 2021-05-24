72a4fe26b0ec92c7eb3d973720001e5720833780
Gabrial <493686444@qq.com>
2021-05-24 21:11:26 +0800

    在EF6上完成上一课作业 over

---
+++ EF6Sample/BBMoneyRegistrar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Sample
+{
+    class BBMoneyRegistrar
+    {
+        SqlDbContext context = new SqlDbContext();
+        public void Change(int payerId, int payeeId, int number)
+        {
+            using (DbContextTransaction transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    User payer = context.Users.Find(payerId);
+                    payer.BBMoney = payer.BBMoney - number;
+                    User payee = context.Users.Find(payeeId);
+                    payee.BBMoney = payee.BBMoney + number;
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ EF6Sample/SqlDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Sample
+{
+    public class SqlDbContext:DbContext
+    {
+        public DbSet<User> Users { set; get; }
+        public SqlDbContext():base("EFsixWork")
+        {
+            Database.Log = Console.WriteLine;
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
