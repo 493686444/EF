448bb2d5f2016a5487b9721267e4f5ec0bd67098
Gabrial <493686444@qq.com>
2021-05-24 08:52:31 +0800

    帮帮币交易  初步完成

---
+++ EF/BBMoneyRegistrar.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF
+{
+    public class BBMoneyRegistrar
+    {
+        SqlDbContext context = new SqlDbContext();
+        public void Change(int payerId,int payeeId, int number)
+        {
+            //EF core：UoW和事务
+            //    用事务实现帮帮币出售的过程
+            //            卖方帮帮币足够，扣减数额后成功提交。 
+            //            卖房帮帮币不够，事务回滚，买卖双方帮帮币不变。 
+           
+            using (IDbContextTransaction transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    User payer = context.Users.Find(payerId);
+                    payer.BBMoney = payer.BBMoney-number;
+                    User payee = context.Users.Find(payeeId);
+                    payee.BBMoney = payee.BBMoney+number;
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
--- EF/User.cs
+++ EF/User.cs
@@ -9,5 +9,6 @@ namespace EF
         public int Id { get; set; } 
         public string Name { get; set; }
         public string Password { get; set; }
+        public int BBMoney { get; set; }
     }
 }
